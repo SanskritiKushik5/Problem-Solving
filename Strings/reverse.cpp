69cf598aebd28ad0c4d823a6b09eb88cd6486d62
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-07-01 09:49:42 +0530

    Create reverse.cpp

---
+++ Strings/reverse.cpp
@@ -0,0 +1,8 @@
+void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+        }
+    }
