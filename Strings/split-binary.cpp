a5ae11858938f3a16eae79a250f42b838977edca
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-07-02 16:21:41 +0530

    Create split-binary.cpp

---
+++ Strings/split-binary.cpp
@@ -0,0 +1,34 @@
+// C++ implementation of the approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubStr(string str, int n)
+{
+	int count0 = 0, count1 = 0;
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (str[i] == '0') {
+			count0++;
+		}
+		else {
+			count1++;
+		}
+		if (count0 == count1) {
+			cnt++;
+		}
+	}
+	if (cnt == 0) {
+		return -1;
+	}
+	return cnt;
+}
+
+int main()
+{
+	string str = "0100110101";
+	int n = str.length();
+
+	cout << maxSubStr(str, n);
+
+	return 0;
+}
