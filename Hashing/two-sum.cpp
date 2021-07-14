35480b0fe138603a0e6ce13b829ca9fe17810836
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-07-15 01:15:54 +0530

    Create two-sum.cpp

---
+++ Hashing/two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if (m.find(target-nums[i])!=m.end()){
+                ans.push_back(m[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            m[nums[i]] = i;
+        }
+        return ans;
+    }
+};
