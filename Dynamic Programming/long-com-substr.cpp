d2a9a331262062abf5b25674f50fa6c94d1322b4
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-07-17 20:01:55 +0530

    Create long-com-substr.cpp

---
+++ Dynamic Programming/long-com-substr.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int longestCommonSubstr (string a, string b, int n, int m)
+    {
+        // your code here
+        int dp[n+1][m+1];
+        int ans = 0;
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0) dp[i][j] = 0;
+                else if(a[i-1]==b[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    ans = max(ans, dp[i][j]);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
