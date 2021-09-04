e403104f0546fa1a8db2ea7f498b0f3082a3f50e
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-09-04 10:38:50 +0530

    Update binary-search.cpp

--- Binary Search/binary-search.cpp
+++ Binary Search/binary-search.cpp
@@ -1,4 +1,3 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <bits/stdc++.h>
 
@@ -12,11 +11,11 @@ int main() {
     int end = n-1;
     while(start<=end){
         int mid = start + (end-start)/2;
-        if(element==mid){
+        if(element==arr[mid]){
             cout << mid;
             break;
         }
-        else if(element<mid)
+        else if(element<arr[mid])
             end = mid - 1;
         else
             start = mid + 1;
