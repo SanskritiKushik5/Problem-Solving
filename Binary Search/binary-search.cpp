3da71a4a9ef4c865df331f53125c6e230bf6cb89
Sanskriti Kushik <sanskritikushik23@gmail.com>
2021-09-04 10:36:05 +0530

    Create binary-search.cpp

---
+++ Binary Search/binary-search.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7;
+    int element = 4;
+    int start = 0;
+    int end = n-1;
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if(element==mid){
+            cout << mid;
+            break;
+        }
+        else if(element<mid)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+
+    return 0;
+}
