44127f3056ee582bbff0b435efef3cb6999ec895
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-03-01 10:16:22 +0530

    Update counting-bits.cpp

--- Leetcode/counting-bits.cpp
+++ Leetcode/counting-bits.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            int num = i;
+            int sum = 0;
+            while(num){
+                sum += num%2;
+                num /= 2;
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
     unsigned int countSetBits(unsigned int n) {
         unsigned int count = 0;
         while (n) {
